f34baea0bae2796d14393a232893050e591d4c05
rizkyanas35 <rizkyanas25@gmail.com>
2019-05-09 22:32:54 +0700

    update

---
+++ client/javascript/components/img-card.js
@@ -0,0 +1,16 @@
+Vue.component('img-card', {
+  data() {
+
+  },
+  
+  template: `
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?cs=srgb&dl=dawn-hd-wallpaper-landscape-36717.jpg&fm=jpg" alt="Card image cap">
+    <div class="card-body">
+      <p class="card-text">Share to :</p>
+      <a href="#" class="btn btn-primary"><i class="fab fa-twitter"></i></a>
+      <a href="#" class="btn btn-primary"><i class="fab fa-facebook-square"></i></a>
+    </div>
+  </div>
+  `
+})
