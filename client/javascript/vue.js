c364144fe3de803df95793a44f55ca1453189fbc
Andre Suchitra <89.andre@gmail.com>
2019-05-10 14:13:48 +0700

    progress

--- client/javascript/vue.js
+++ client/javascript/vue.js
@@ -8,7 +8,6 @@ const app = new Vue({
     ClipLoader,
   },
   data : {
-
     quote : '',
     textColor : '#ffffff',
     weight: 'normal',
@@ -16,15 +15,14 @@ const app = new Vue({
     x: 0,
     y: 0,
     file: [],
-
-    quote : null,
     isLoading: false,
+    image: '',
   },
   mounted() {
     this.generateQuote()
 
   },
-  methods : {
+  methods: {
     generateQuote() {
       this.isLoading = true;
       this.quote = null
@@ -70,8 +68,53 @@ const app = new Vue({
       if(input == 'left') this.y-=10
       console.log(this.x, this.y)
     },
-  },
+    
+     onFileChange(e){
+         let files = e.target.files || e.dataTransfer.files;
+             if (!files.length)
+                 return;
+             this.createImage(files[0]);
+             this.file = this.$refs.file.files[0]
+     },
+     createImage(file) {
+         let reader = new FileReader();
+         reader.onload = (e) => {
+             this.image = e.target.result;
+             console.log(this.image);
+         };
+         reader.readAsDataURL(file);
+     },
+     upload(){
+         let fileString = this.image.toString()
+         let name = this.file.name.substring(0, this.file.name.indexOf('.'))
 
+         console.log(fileString);
+         console.log('======', name);
+         let extension =  '.' + fileString.substring(fileString.indexOf('/')+1, fileString.indexOf(';'))
+
+         if(extension === '.') {
+           extension = '.png'
+         }
+
+         axios({
+             url: 'http://localhost:3000/upload',
+             method: 'POST',
+             data: {
+                 image: this.image,
+                 name: name,
+                 extension: extension,
+             }
+         })
+         .then(({data})=> {
+             console.log(data.link)
+             this.image= ''
+             this.file = ''
+         })
+         .catch(err =>{
+             console.log(err)
+         })
+     }
+  },
   computed : {
 
   },
