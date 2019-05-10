e3fa4e652d0ed035f8e0a1f6926db2dd430fe3f7
Andre Suchitra <89.andre@gmail.com>
2019-05-10 14:13:48 +0700

    progress

--- client/javascript/vue.js
+++ client/javascript/vue.js
@@ -2,10 +2,10 @@ const app = new Vue({
   el : '#app',
   data : {
     quote : '',
-
+    image: '',
+    file: '',
   },
-
-  methods : {
+  methods: {
     generateQuote() {
       axios({
         method: 'get',
@@ -17,9 +17,53 @@ const app = new Vue({
       .catch(err => {
         console.log(err)
       })
-    }
-  },
+    },
+     onFileChange(e){
+         let files = e.target.files || e.dataTransfer.files;
+             if (!files.length)
+                 return;
+             this.createImage(files[0]);
+             this.file = this.$refs.file.files[0]
+     },
+     createImage(file) {
+         let reader = new FileReader();
+         reader.onload = (e) => {
+             this.image = e.target.result;
+             console.log(this.image);
+         };
+         reader.readAsDataURL(file);
+     },
+     upload(){
+         let fileString = this.image.toString()
+         let name = this.file.name.substring(0, this.file.name.indexOf('.'))
 
+         console.log(fileString);
+         console.log('======', name);
+         let extension =  '.' + fileString.substring(fileString.indexOf('/')+1, fileString.indexOf(';'))
+
+         if(extension === '.') {
+           extension = '.png'
+         }
+
+         axios({
+             url: 'http://localhost:3000/upload',
+             method: 'POST',
+             data: {
+                 image: this.image,
+                 name: name,
+                 extension: extension,
+             }
+         })
+         .then(({data})=> {
+             console.log(data.link)
+             this.image= ''
+             this.file = ''
+         })
+         .catch(err =>{
+             console.log(err)
+         })
+     }
+  },
   computed : {
 
   },
